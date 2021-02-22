cb5f8e2d02db5dbe9593bfd50b1899f0ffc865ea
Thomas Adamson <thomasadamson@Thomass-MacBook-Air-2.local>
2021-02-22 14:41:05 -0800

    Finished final two queries.

--- queries.sql
+++ queries.sql
@@ -58,8 +58,19 @@ WHERE d.dept_name = 'Sales';
 --including their employee number, last name, first name, 
 --and department name.
 
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de ON
+e.emp_no = de.emp_no
+JOIN departments AS d ON
+d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 
 --In descending order, list the frequency count of employee 
 --last names, i.e., how many employees share each last name.
 
+SELECT last_name, COUNT(*) AS "lastname_count"
+FROM employees
+GROUP BY last_name
+ORDER BY lastname_count DESC;
